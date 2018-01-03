da60d3e71cf84b360a1b6fdd5794c61c6c0fdfd2
Anna Royer <anna.royer@colorado.edu>
2018-01-03 09:16:14 -0700

    add activity test and class methods

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,17 @@
 class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(name, amount_paid)
+    @participants[name] = amount_paid
+  end
+
+  def total_cost
+    @participants.values.reduce(0, :+)
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,49 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_has_a_name
+    activity = Activity.new('hiking')
+
+    assert_equal 'hiking', activity.name
+  end
+
+  def test_it_has_participants
+    activity = Activity.new('hiking')
+
+    assert activity.participants
+  end
+
+  def test_it_can_add_participants
+    activity = Activity.new('hiking')
+
+    assert activity.participants.empty?
+
+    activity.add_participant('William', 0)
+
+    assert ({'William' => 0.00}), activity.participants
+  end
+
+  def test_you_can_add_multiple_participants
+    activity = Activity.new('roller skating')
+    activity.add_participant('William', 5.00)
+    activity.add_participant('Sabrina', 5.00)
+    activity.add_participant('Ella', 7.00)
+
+    assert_equal 3, activity.participants.count
+  end
+
+  def test_it_can_evaluate_the_total_cost_of_an_activity
+    activity = Activity.new('roller skating')
+    activity.add_participant('William', 5.00)
+    activity.add_participant('Sabrina', 5.00)
+    activity.add_participant('Ella', 7.00)
+
+    assert_equal 17.00, activity.total_cost
+  end
+
+  def test_total_cost_starts_at_0
+    activity = Activity.new('hiking')
+
+    assert_equal 0, activity.total_cost
+  end
 end
