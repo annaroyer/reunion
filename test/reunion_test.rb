b389c1e427c05092bee51623b8b8aed8185bdbf4
Anna Royer <anna.royer@colorado.edu>
2018-01-03 09:46:17 -0700

    add iteration 3

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,7 +1,26 @@
 require 'minitest'
-require 'minitest/test'
+require 'minitest/pride'
 require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+  def test_it_has_a_location
+    reunion = Reunion.new('South Carolina')
+
+    assert_equal 'South Carolina', reunion.location
+  end
+
+  def test_it_has_activities
+    reunion = Reunion.new('South Carolina')
+
+    assert reunion.activities
+  end
+
+  def test_it_can_add_activities
+    reunion = Reunion.new('South Carolina')
+    assert reunion.activities.empty?
+    reunion.add_activity('hiking')
+
+    assert reunion.activities.include?('hiking')
+  end
 end
